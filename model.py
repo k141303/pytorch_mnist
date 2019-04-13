034efaba8399cceff249773883ab454a5e75bf3f
中山功太 <nakayamakouta@nakayamakoutanoMacBook-Air.local>
2019-04-13 18:40:58 +0900

    層の追加

--- model.py
+++ model.py
@@ -8,6 +8,11 @@ import torch.nn.functional as F
 class Net(torch.nn):
     def __init__(self,hidden):
         super(Net, self).__init__()
+        self.hidden = hidden
+        self.fc1 = nn.Linear(hidden,hidden)
+        self.fc1 = nn.Linear(hidden,10)
 
     def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
