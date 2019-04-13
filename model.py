d16e70f167f5aba0f960969c22302a6eeadc29e9
中山功太 <nakayamakouta@nakayamakoutanoMacBook-Air.local>
2019-04-13 17:20:14 +0900

    モデルの枠組みができた

---
+++ model.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(torch.nn):
+    def __init__(self,hidden):
+        super(Net, self).__init__()
+
+    def forward(self,x):
+        return x
