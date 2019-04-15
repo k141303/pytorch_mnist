c6fae5ba6b3bc0bcd66b3bd32419e0aa8d911ce9
中山功太 <nakayamakouta@nakayamakoutanoMacBook-Air.local>
2019-04-15 12:50:02 +0900

    モデルにコメントの追加

--- model.py
+++ model.py
@@ -8,12 +8,12 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self,hidden = 256):
         super(Net, self).__init__()
-        self.hidden = hidden
-        self.fc1 = nn.Linear(28*28,hidden)
-        self.fc2 = nn.Linear(hidden,10)
+        self.hidden = hidden    #隠れ層のサイズ
+        self.fc1 = nn.Linear(28*28,hidden)  #入力層
+        self.fc2 = nn.Linear(hidden,10) #出力層
 
     def forward(self,x):
-        x = x.view(-1,28*28)
+        x = x.view(-1,28*28)    #入力を(バッチサイズ,1,28,28)から(バッチサイズ,28*28)に変換する
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
